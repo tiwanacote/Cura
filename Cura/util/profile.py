6c53fbe7d93975b45c8f53ef0e4f2be5645e67c5
rulochaparro <chaparroraulantonio@gmail.com>
2018-02-26 14:15:50 -0300

    Prueba

--- Cura/util/profile.py
+++ Cura/util/profile.py
@@ -344,7 +344,7 @@ G1 F7200
 M117 Printing...
         
 
-""", str, 'alteration', 'machine')
+""", str, 'alteration', 'alteration_0')
 #######################################################################################
 setting('end.gcode', """;End GCode
 M104 S0
@@ -357,9 +357,9 @@ G90                         ;absolute positioning
 M190 S0.000000
 M109 S0.000000
 
-""", str, 'alteration', 'machine')
+""", str, 'alteration', 'alteration_0')
 #######################################################################################
-setting('start1.gcode', """; TRIMAKER COSMOS Start GCode 
+setting('start.gcode', """; TRIMAKER COSMOS Start GCode 
 G21      			; Metric values
 G28 				; Home axis
 G1 X0 Y0 Z15
@@ -379,9 +379,9 @@ G1 F2000
 ; Put printing message on LCD screen
 M117 Imprimiendo
 			
-""", str, 'alteration', 'machine')
+""", str, 'alteration', 'alteration_1')
 #######################################################################################
-setting('end1.gcode', """;End GCode
+setting('end.gcode', """;End GCode
 G90 				; Set to absolute positioning
 G1 X0 Y0 Z130		; Get extruder out of way
 G92 E0			 	; Reset extruder position
@@ -392,7 +392,7 @@ M104 S0 			; Disable extruder
 M84 				; Turn steppers off
 M107			; Fan off
 M117 Impresion finalizada
-""", str, 'alteration', 'machine')
+""", str, 'alteration', 'alteration_1')
 #######################################################################################
 setting('start3.gcode', """;Sliced at: {day} {date} {time}
 ;Basic settings: Layer height: {layer_height} Walls: {wall_thickness} Fill: {fill_density}
